813d9527622c0babf0c19f46bdfd5997d5a005ba
qcamiloq <qcamiloq@gmail.com>
2016-05-07 18:59:55 -0500

    Scena inicial definida, se carga el tornillo, el stl de la columna y las imagenes medicas, ademas queda acoplado el stl con las imagenes

--- moduloEntrenador.py
+++ moduloEntrenador.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from __main__ import vtk, qt, ctk, slicer
 
 
@@ -32,12 +33,40 @@ class moduloEntrenadorWidget:
 
   def setup(self):
 
+    self.cargarScene()
+
     sampleCollapsibleButton = ctk.ctkCollapsibleButton()
-    sampleCollapsibleButton.text = "A collapsible button"
+    sampleCollapsibleButton.text = "A collapsible button1"
     self.layout.addWidget(sampleCollapsibleButton)
 
     
     sampleFormLayout = qt.QFormLayout(sampleCollapsibleButton)
+
+  def cargarScene(self):
+
+    path1='C:\Users\Camilo_Q\Documents\GitHub\moduloEntrenador/stlcolumna.stl' #Se obtiene direccion de la unbicación del tornillo
+    path2='C:\Users\Camilo_Q\Documents\GitHub\moduloEntrenador\Lumbar 2.5 B31s - 4/4 Lumbar  2.5  B31s.nrrd'
+    slicer.util.loadModel(path1)
+    slicer.util.loadVolume(path2)
+
+    columna=slicer.util.getNode('stlcolumna') # Se obtiene el nodo del objeto en escena
+    columnaModelDisplayNode = columna.GetDisplayNode() 
+    columnaModelDisplayNode.SetColor(1,1,3) 
+    columnaModelDisplayNode.SetSliceIntersectionVisibility(1)
+    transformadaColumna=slicer.vtkMRMLLinearTransformNode()
+    transformadaColumna.SetName('Transformada')
+    slicer.mrmlScene.AddNode(transformadaColumna)
+    columna.SetAndObserveTransformNodeID(transformadaColumna.GetID()) #Asocia el objeto a la matriz
+    mt = vtk.vtkMatrix4x4()
+    transformadaColumna.GetMatrixTransformToParent(mt)
+    mt.SetElement(0,0,-1) #Asigno el origen de la matriz en el fiducial target para mover el tornillo y rotar sobre este punto
+    mt.SetElement(1,1,-1)
+    transformadaColumna.SetAndObserveMatrixTransformToParent(mt)
+    layoutManager = slicer.app.layoutManager() 
+    threeDWidget = layoutManager.threeDWidget(0)
+    threeDView = threeDWidget.threeDView()
+    threeDView.resetFocalPoint()
+
 
 
 
